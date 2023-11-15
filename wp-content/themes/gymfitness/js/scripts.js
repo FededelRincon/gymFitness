fce3d7426870d34e04b9f5c7f2ec97bca576f147
FededelRincon <delrincon@gmail.com>
2023-11-15 19:56:04 -0300

    fix codigo js de animacion de letras en el resto de paginas

--- wp-content/themes/gymfitness/js/scripts.js
+++ wp-content/themes/gymfitness/js/scripts.js
@@ -13,24 +13,28 @@ function gymWordPress(){
     // animacion de letras
     // Wrap every letter in a span
     let textWrapper = document.querySelector('.ml2');
-    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-    anime.timeline({loop: true})
-    .add({
-        targets: '.ml2 .letter',
-        scale: [4,1],
-        opacity: [0,1],
-        translateZ: 0,
-        easing: "easeOutExpo",
-        duration: 950,
-        delay: (el, i) => 70*i
-    }).add({
-        targets: '.ml2',
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 1000
-    });
+    // Verifica si el elemento existe antes de continuar
+    if (textWrapper !== null) {
+        textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+        anime.timeline({loop: true})
+        .add({
+            targets: '.ml2 .letter',
+            scale: [4,1],
+            opacity: [0,1],
+            translateZ: 0,
+            easing: "easeOutExpo",
+            duration: 950,
+            delay: (el, i) => 70*i
+        }).add({
+            targets: '.ml2',
+            opacity: 0,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000
+        });
+    }
 }
 
 document.addEventListener('DOMContentLoaded', gymWordPress);
